4a4c61436f5069fb3186f46e7f68229cf10560da
Alex Anderson <alexandersonone@gmail.com>
2019-04-28 01:28:52 -0700

    Add a json template

--- src/crud.go
+++ src/crud.go
@@ -1,7 +1,38 @@
 package main
 
+import (
+	_ "encoding/json"
+)
+
+type overview struct {
+	Name    string
+	Id      string
+	Service string
+}
+
+type provider struct {
+	Name    string
+	Phone   string
+	Email   string
+	Address string
+	Coords  string
+	Website string
+	Id      string
+	Service string
+}
+
+type results struct {
+	Shelter   []overview
+	Food      []overview
+	Transport []overview
+	Medical   []overview
+}
+
 // Get available resources for a person
-func getAvailable(service, gender, age, vet, family string) string {
+func getAvailable(service, sex, age, vet, family string, top int) string {
+	if service == "any" {
+
+	}
 
 	// stuff here
 
@@ -10,5 +41,6 @@ func getAvailable(service, gender, age, vet, family string) string {
 
 // Get more information about a specific resource
 func getProvider(id string) string {
+
 	return "{}"
 }
